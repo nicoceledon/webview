172d4e2fc7266fc8a7ead150b1146c1ebf738136
Nicolas Celedon <nicoceledon@MacBook-Pro.local>
2020-07-11 11:31:25 -0400

    init commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {WebView} from 'react-native-webview';
+
+const App = () => {
+  return <WebView source={{uri: 'YOUR_URI'}} />;
+};
+
+export default App;
